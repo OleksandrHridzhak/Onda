fa0ba7879ddfd5be4bf08b9a99633f17b4c3d041
HridzhakOleksandr <oleksandr.hridzhak@lnu.edu.ua>
2025-08-18 19:16:43 +0300

    added basic slice for table

--- render/src/store/index.js
+++ render/src/store/index.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import pomodoroReducer from './slices/pomodoroSlice';
 import themeReducer from './slices/themeSlice';
+import tableReducer from './table/tableSlice';
 
 export const store = configureStore({
   reducer: {
     pomodoro: pomodoroReducer,
     theme: themeReducer,
+    table: tableReducer,
   },
 });
 
-export default store; 
+export default store;
