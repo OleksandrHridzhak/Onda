12484436349463e796b8be6a3556f9711a939f2f
OleksandrHridzhak <sgridzak@gmail.com>
2025-10-31 18:11:16 +0200

    refactor: update theme state management and remove old theme slice

--- render/src/components/settings/sections/UiSection.jsx
+++ render/src/components/settings/sections/UiSection.jsx
@@ -3,7 +3,7 @@ import SettingsTemplate from '../SettingsTemplate';
 import { useSelector } from 'react-redux';
 
 export default function UISection({ settings, onThemeChange, onAutoThemeChange }) {
-  const { mode } = useSelector((state) => state.theme);
+  const { mode } = useSelector((state) => state.newTheme);
   const darkTheme = mode === 'dark';
 
   const handleAccentColorChange = (e) => {
@@ -38,42 +38,6 @@ export default function UISection({ settings, onThemeChange, onAutoThemeChange }
           <option value="ocean">Ocean</option>
         </select>
       </div>
-
-
-      {settings.theme.autoThemeSettings.enabled && (
-        <div className="mt-4 space-y-3">
-          <div className="flex items-center justify-between">
-            <label className={`text-sm text-textTableValues`}>
-              Start of Day
-            </label>
-            <input
-              type="time"
-              value={settings.theme.autoThemeSettings.startTime}
-              onChange={(e) => onAutoThemeChange({ startTime: e.target.value })}
-              className={`w-32 rounded-md text-m px-2 py-1 ${
-                darkTheme
-                  ? 'bg-gray-800 text-gray-200 border border-gray-700'
-                  : 'bg-white text-gray-600 border border-gray-300'
-              }`}
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <label className={`text-sm text-textTableValues`}>
-              End of Day
-            </label>
-            <input
-              type="time"
-              value={settings.theme.autoThemeSettings.endTime}
-              onChange={(e) => onAutoThemeChange({ endTime: e.target.value })}
-              className={`w-32 rounded-md text-m px-2 py-1 ${
-                darkTheme
-                  ? 'bg-gray-800 text-gray-200 border border-gray-700'
-                  : 'bg-white text-gray-600 border border-gray-300'
-              }`}
-            />
-          </div>
-        </div>
-      )}
     </SettingsTemplate>
   );
 }
