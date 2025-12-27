94e0d2ab855878de04266352f3172d8709e6964a
OleksandrHridzhak <sgridzak@gmail.com>
2025-12-27 18:09:03 +0200

    refactor: format keywords in package.json and import dotenv in server.js

--- sync-server/server.js
+++ sync-server/server.js
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
