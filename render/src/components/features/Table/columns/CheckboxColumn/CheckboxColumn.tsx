80b8ed78351215124afc000f869e9570e5587f3f
OleksandrHridzhak <sgridzak@gmail.com>
2026-01-17 09:37:46 +0200

    refactor: extract DayColumnLayout component for better structure

--- render/src/components/features/Table/columns/CheckboxColumn/CheckboxColumn.tsx
+++ render/src/components/features/Table/columns/CheckboxColumn/CheckboxColumn.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { ColumnHeader } from '../ColumnHeader';
 import { CheckboxCell } from './CheckBoxCell';
-import { DAYS } from '../../TableLogic';
 import { updateColumnFields } from '../../../../../db/helpers/columns';
 import { CheckBoxColumn } from '../../../../../types/newColumn.types';
 import { useReactiveColumn } from '../../hooks/useReactiveColumn';
+import { DayColumnLayout } from '../DayColumnLayout';
 
 interface CheckboxColumnProps {
     columnId: string;
 }
+/**
+ * Just a checkbox column.
+ */
 
 export const CheckboxColumn: React.FC<CheckboxColumnProps> = ({ columnId }) => {
     const { column, isLoading, isError } = useReactiveColumn<CheckBoxColumn>(
@@ -28,34 +31,17 @@ export const CheckboxColumn: React.FC<CheckboxColumnProps> = ({ columnId }) => {
     return (
         <table className="checkbox-nested-table column-checkbox font-poppins">
             <ColumnHeader columnId={columnId} />
-
-            <tbody className="bg-tableBodyBg">
-                {DAYS.map((day, idx) => (
-                    <tr
-                        key={day}
-                        className={
-                            idx !== DAYS.length - 1
-                                ? 'border-b border-border'
-                                : ''
+            <DayColumnLayout>
+                {(day) => (
+                    <CheckboxCell
+                        checked={column?.uniqueProps?.days?.[day] || false}
+                        onChange={(newValue) =>
+                            handleCheckboxChange(day, newValue)
                         }
-                    >
-                        <td>
-                            <CheckboxCell
-                                checked={
-                                    column?.uniqueProps?.days?.[day] || false
-                                }
-                                onChange={(newValue) =>
-                                    handleCheckboxChange(day, newValue)
-                                }
-                                color={
-                                    column?.uniqueProps?.checkboxColor ||
-                                    'green'
-                                }
-                            />
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
+                        color={column?.uniqueProps?.checkboxColor || 'green'}
+                    />
+                )}
+            </DayColumnLayout>
         </table>
     );
 };
---
+++ render/src/components/features/Table/columns/DayColumnLayout.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { DAYS } from '../TableLogic';
+
+interface DayColumnLayoutProps {
+    children: (day: string) => React.ReactNode;
+}
+/**
+ * Layout component for rendering rows for each day of the week.
+ * Accepts a render prop to customize the content of each day's cell.
+ *
+ * @why Provides a reusable layout for day-based columns in tables.
+ *
+ *  */
+export const DayColumnLayout: React.FC<DayColumnLayoutProps> = ({
+    children,
+}) => {
+    return (
+        <tbody className="bg-tableBodyBg">
+            {DAYS.map((day) => (
+                <tr key={day} className="border-b border-border last:border-0">
+                    <td className="h-full w-full">{children(day)}</td>
+                </tr>
+            ))}
+        </tbody>
+    );
+};
